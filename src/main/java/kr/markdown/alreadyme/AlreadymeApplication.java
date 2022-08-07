1cdb9ad0e2db810998cdc7bd1209c1e3d8b22629
npole0103 <npole0103@naver.com>
2022-08-07 23:29:06 +0900

    feat: Initialize Spring Boot

---
+++ src/main/java/kr/markdown/alreadyme/AlreadymeApplication.java
@@ -0,0 +1,13 @@
+package kr.markdown.alreadyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlreadymeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlreadymeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/markdown/alreadyme/AlreadymeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.markdown.alreadyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlreadymeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
