9265bc000e241f746740b89f201dc253b80acf72
npole0103 <npole0103@naver.com>
2022-09-06 01:46:50 +0900

    feat: Add getReadmeText method prototype in AiService

--- src/main/java/kr/markdown/alreadyme/service/AiService.java
+++ src/main/java/kr/markdown/alreadyme/service/AiService.java
@@ -1,4 +1,18 @@
 package kr.markdown.alreadyme.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AiService {
+
+    public String getReadmeText(String jsonString) {
+
+        //httpClient
+
+        //response.toString()
+
+        return "create readme.md success";
+    }
 }
