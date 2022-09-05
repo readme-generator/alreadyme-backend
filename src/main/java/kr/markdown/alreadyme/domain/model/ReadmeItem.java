64883bea02a5c081d62ec7b49b444ff601fc11cc
npole0103 <npole0103@naver.com>
2022-09-06 01:52:00 +0900

    feat: Delete githubBotUrl & Add readmeText column MEDIUMTEXT

--- src/main/java/kr/markdown/alreadyme/domain/model/ReadmeItem.java
+++ src/main/java/kr/markdown/alreadyme/domain/model/ReadmeItem.java
@@ -3,10 +3,7 @@ package kr.markdown.alreadyme.domain.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
@@ -23,12 +20,10 @@ public class ReadmeItem {
     private Long id;
 
     @NotBlank
-    private String githubUrl;
+    private String githubOriginalUrl;
 
     @NotBlank
-    private String githubBotUrl;
-
-    @NotBlank
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String readmeText;
 
     private String objectUrl;
