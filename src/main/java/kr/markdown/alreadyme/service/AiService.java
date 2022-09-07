f9022fa7005c54bcc89ce252417709d17b36eb4f
npole0103 <npole0103@naver.com>
2022-09-07 13:16:25 +0900

    feat: Add githubOriginalUrl to AiServerRequest

--- src/main/java/kr/markdown/alreadyme/service/AiService.java
+++ src/main/java/kr/markdown/alreadyme/service/AiService.java
@@ -1,13 +1,26 @@
 package kr.markdown.alreadyme.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AiService {
 
-    public String getReadmeText(String jsonString) {
+    public String getReadmeText(String requestJsonData, String githubOriginalUrl) throws JsonProcessingException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("githubOriginalUrl", githubOriginalUrl);
+
+        ObjectNode jsonDataNode = (ObjectNode) new ObjectMapper().readTree(requestJsonData);
+        objectNode.set("data", jsonDataNode);
 
         //httpClient
 
